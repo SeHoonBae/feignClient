b71a3197d5d06d2a147bd66c34bbce785f675c61
SeHoonBae <bey1548@naver.com>
2022-06-09 14:03:47 +0900

    Users Entity mapstruct사용을 위한 생성자 레벨 제거

--- src/main/java/com/feign/feignserver/dto/BoardDto.java
+++ src/main/java/com/feign/feignserver/dto/BoardDto.java
@@ -6,10 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BoardDto {
-
     private Long id;
     private String title;
     private String content;
     private String userId;
-
 }
--- src/main/java/com/feign/feignserver/entity/Users.java
+++ src/main/java/com/feign/feignserver/entity/Users.java
@@ -1,24 +1,17 @@
 package com.feign.feignserver.entity;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter @Setter
 public class Users {
-
     @Id
     private String userId;
-
     @Column
     private String userName;
-
 }
